f6efeae9031c1b416eb49585c918c22ed86774f7
YOUN KUE CHOI <daybrush@gmail.com>
2019-11-11 13:58:01 +0900

    Updates

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,9 @@ name: @scena/guides
 license: MIT
 author: Daybrush
 repository: git+https://github.com/daybrush/guides.git
-version: 0.1.1
+version: 0.1.2
 */
-(function (styled) {
+(function () {
     'use strict';
 
     /*! *****************************************************************************
@@ -121,7 +121,7 @@ version: 0.1.1
 
     var r$1,u$1=[],i$1=n.__r;n.__r=function(n){i$1&&i$1(n),(r$1=n.__c).__H&&(r$1.__H.t.forEach(g$1),r$1.__H.t.forEach(q),r$1.__H.t=[]);};var o$1=n.diffed;n.diffed=function(n){o$1&&o$1(n);var t=n.__c;if(t){var r=t.__H;r&&r.t.length&&A$1(u$1.push(t));}};var f$1=n.__c;n.__c=function(n,t){t.some(function(n){n.__h.forEach(g$1),n.__h=n.__h.filter(function(n){return !n.u||q(n)});}),f$1&&f$1(n,t);};var c$1=n.unmount;n.unmount=function(n){c$1&&c$1(n);var t=n.__c;if(t){var r=t.__H;r&&r.i.forEach(function(n){return n.m&&n.m()});}};var A$1=function(){};function F(){u$1.some(function(n){n.__P&&(n.__H.t.forEach(g$1),n.__H.t.forEach(q),n.__H.t=[]);}),u$1=[];}if("undefined"!=typeof window){var _$1=n.requestAnimationFrame;A$1=function(t){1!==t&&_$1===n.requestAnimationFrame||((_$1=n.requestAnimationFrame)||function(n){var t=function(){clearTimeout(r),cancelAnimationFrame(u),setTimeout(n);},r=setTimeout(t,100),u=requestAnimationFrame(t);})(F);};}function g$1(n){n.m&&n.m();}function q(n){var t=n.u();"function"==typeof t&&(n.m=t);}
 
-    function E(n){var t=n.parentNode;t&&t.removeChild(n);}var _$2=n.__e;function k$1(n){this.__u=[],this.__f=n.fallback;}n.__e=function(n,t,e){if(n.then&&e)for(var r,o=t;o=o.__p;)if((r=o.__c)&&r.o)return e&&(t.__e=e.__e,t.__k=e.__k),void r.o(n);_$2(n,t,e);},(k$1.prototype=new y).o=function(n){var t=this;t.__u.push(n);var e=function(){t.__u[t.__u.indexOf(n)]=t.__u[t.__u.length-1],t.__u.pop(),0==t.__u.length&&(t.__f&&A(t.__f),t.__v.__e=null,t.__v.__k=t.state.u,t.setState({u:null}));};null==t.state.u&&(t.__f=t.__f&&L(t.__f),t.setState({u:t.__v.__k}),function n(t){for(var e=0;e<t.length;e++){var r=t[e];null!=r&&("function"!=typeof r.type&&r.__e?E(r.__e):r.__k&&n(r.__k));}}(t.__v.__k),t.__v.__k=[]),n.then(e,e);},k$1.prototype.render=function(n,t){return t.u?this.__f:n.children};var S="undefined"!=typeof Symbol&&Symbol.for&&Symbol.for("react.element")||60103,F$1=/^(?:accent|alignment|arabic|baseline|cap|clip|color|fill|flood|font|glyph|horiz|marker|overline|paint|stop|strikethrough|stroke|text|underline|unicode|units|v|vector|vert|word|writing|x)[A-Z]/,N$1=n.event;n.event=function(n){return N$1&&(n=N$1(n)),n.persist=function(){},n.nativeEvent=n};var M=function(){};function O(n){var t=this,e=n.container,r=h(M,{context:t.context},n.vnode);return t.i&&t.i!==e&&(t.l.parentNode&&t.i.removeChild(t.l),A(t.s),t.v=!1),n.vnode?t.v?(e.__k=t.__k,H(r,e),t.__k=e.__k):(t.l=document.createTextNode(""),I("",e),e.appendChild(t.l),t.v=!0,t.i=e,H(r,e,t.l),t.__k=this.l.__k):t.v&&(t.l.parentNode&&t.i.removeChild(t.l),A(t.s)),t.s=r,t.componentWillUnmount=function(){t.l.parentNode&&t.i.removeChild(t.l),A(t.s);},null}function j$1(n,t){return h(O,{vnode:n,container:t})}M.prototype.getChildContext=function(){return this.props.context},M.prototype.render=function(n){return n.children};function W(){for(var n=[],t=arguments.length;t--;)n[t]=arguments[t];var e=h.apply(void 0,n),r=e.type,o=e.props;return "function"!=typeof r&&(o.defaultValue&&(o.value||0===o.value||(o.value=o.defaultValue),delete o.defaultValue),Array.isArray(o.value)&&o.multiple&&"select"===r&&(b(o.children).forEach(function(n){-1!=o.value.indexOf(n.props.value)&&(n.props.selected=!0);}),delete o.value),function(n,t){var e,r,o;for(o in t)if(e=F$1.test(o))break;if(e)for(o in r=n.props={},t)r[F$1.test(o)?o.replace(/([A-Z0-9])/,"-$1").toLowerCase():o]=t[o];}(e,o)),e.preactCompatNormalized=!1,D$1(e)}function D$1(n){return n.preactCompatNormalized=!0,function(n){var t=n.props;(t.class||t.className)&&(H$1.enumerable="className"in t,t.className&&(t.class=t.className),Object.defineProperty(t,"className",H$1));}(n),n}var H$1={configurable:!0,get:function(){return this.class}};function I$1(n,t){for(var e in n)if("__source"!==e&&!(e in t))return !0;for(var r in t)if("__source"!==r&&n[r]!==t[r])return !0;return !1}var $$1=function(n){function t(t){n.call(this,t),this.isPureReactComponent=!0;}return n&&(t.__proto__=n),(t.prototype=Object.create(n&&n.prototype)).constructor=t,t.prototype.shouldComponentUpdate=function(n,t){return I$1(this.props,n)||I$1(this.state,t)},t}(y);function G(n,t){n["UNSAFE_"+t]&&!n[t]&&Object.defineProperty(n,t,{configurable:!1,get:function(){return this["UNSAFE_"+t]},set:function(n){this["UNSAFE_"+t]=n;}});}y.prototype.isReactComponent={};var J=n.vnode;n.vnode=function(n){n.$$typeof=S,function(t){var e=n.type,r=n.props;if(r&&"string"==typeof e){var o={};for(var u in r)/^on(Ani|Tra)/.test(u)&&(r[u.toLowerCase()]=r[u],delete r[u]),o[u.toLowerCase()]=u;if(o.ondoubleclick&&(r.ondblclick=r[o.ondoubleclick],delete r[o.ondoubleclick]),o.onbeforeinput&&(r.onbeforeinput=r[o.onbeforeinput],delete r[o.onbeforeinput]),o.onchange&&("textarea"===e||"input"===e.toLowerCase()&&!/^fil|che|ra/i.test(r.type))){var i=o.oninput||"oninput";r[i]||(r[i]=r[o.onchange],delete r[o.onchange]);}}}();var t=n.type;t&&t.t&&n.ref&&(n.props.ref=n.ref,n.ref=null),"function"==typeof t&&!t.p&&t.prototype&&(G(t.prototype,"componentWillMount"),G(t.prototype,"componentWillReceiveProps"),G(t.prototype,"componentWillUpdate"),t.p=!0),J&&J(n);};
+    function E(n){var t=n.parentNode;t&&t.removeChild(n);}var _$2=n.__e;function k$1(n){this.__u=[],this.__f=n.fallback;}n.__e=function(n,t,e){if(n.then&&e)for(var r,o=t;o=o.__p;)if((r=o.__c)&&r.o)return e&&(t.__e=e.__e,t.__k=e.__k),void r.o(n);_$2(n,t,e);},(k$1.prototype=new y).o=function(n){var t=this;t.__u.push(n);var e=function(){t.__u[t.__u.indexOf(n)]=t.__u[t.__u.length-1],t.__u.pop(),0==t.__u.length&&(t.__f&&A(t.__f),t.__v.__e=null,t.__v.__k=t.state.u,t.setState({u:null}));};null==t.state.u&&(t.__f=t.__f&&L(t.__f),t.setState({u:t.__v.__k}),function n(t){for(var e=0;e<t.length;e++){var r=t[e];null!=r&&("function"!=typeof r.type&&r.__e?E(r.__e):r.__k&&n(r.__k));}}(t.__v.__k),t.__v.__k=[]),n.then(e,e);},k$1.prototype.render=function(n,t){return t.u?this.__f:n.children};var S="undefined"!=typeof Symbol&&Symbol.for&&Symbol.for("react.element")||60103,F$1=/^(?:accent|alignment|arabic|baseline|cap|clip|color|fill|flood|font|glyph|horiz|marker|overline|paint|stop|strikethrough|stroke|text|underline|unicode|units|v|vector|vert|word|writing|x)[A-Z]/,N$1=n.event;n.event=function(n){return N$1&&(n=N$1(n)),n.persist=function(){},n.nativeEvent=n};var M=function(){};function O(n){var t=this,e=n.container,r=h(M,{context:t.context},n.vnode);return t.i&&t.i!==e&&(t.l.parentNode&&t.i.removeChild(t.l),A(t.s),t.v=!1),n.vnode?t.v?(e.__k=t.__k,H(r,e),t.__k=e.__k):(t.l=document.createTextNode(""),I("",e),e.appendChild(t.l),t.v=!0,t.i=e,H(r,e,t.l),t.__k=this.l.__k):t.v&&(t.l.parentNode&&t.i.removeChild(t.l),A(t.s)),t.s=r,t.componentWillUnmount=function(){t.l.parentNode&&t.i.removeChild(t.l),A(t.s);},null}function j$1(n,t){return h(O,{vnode:n,container:t})}M.prototype.getChildContext=function(){return this.props.context},M.prototype.render=function(n){return n.children};function W(){for(var n=[],t=arguments.length;t--;)n[t]=arguments[t];var e=h.apply(void 0,n),r=e.type,o=e.props;return "function"!=typeof r&&(o.defaultValue&&(o.value||0===o.value||(o.value=o.defaultValue),delete o.defaultValue),Array.isArray(o.value)&&o.multiple&&"select"===r&&(b(o.children).forEach(function(n){-1!=o.value.indexOf(n.props.value)&&(n.props.selected=!0);}),delete o.value),function(n,t){var e,r,o;for(o in t)if(e=F$1.test(o))break;if(e)for(o in r=n.props={},t)r[F$1.test(o)?o.replace(/([A-Z0-9])/,"-$1").toLowerCase():o]=t[o];}(e,o)),e.preactCompatNormalized=!1,D$1(e)}function D$1(n){return n.preactCompatNormalized=!0,function(n){var t=n.props;(t.class||t.className)&&(H$1.enumerable="className"in t,t.className&&(t.class=t.className),Object.defineProperty(t,"className",H$1));}(n),n}var H$1={configurable:!0,get:function(){return this.class}};function I$1(n,t){for(var e in n)if("__source"!==e&&!(e in t))return !0;for(var r in t)if("__source"!==r&&n[r]!==t[r])return !0;return !1}function T$1(n){return n&&(n.base||1===n.nodeType&&n)||null}var $$1=function(n){function t(t){n.call(this,t),this.isPureReactComponent=!0;}return n&&(t.__proto__=n),(t.prototype=Object.create(n&&n.prototype)).constructor=t,t.prototype.shouldComponentUpdate=function(n,t){return I$1(this.props,n)||I$1(this.state,t)},t}(y);function G(n,t){n["UNSAFE_"+t]&&!n[t]&&Object.defineProperty(n,t,{configurable:!1,get:function(){return this["UNSAFE_"+t]},set:function(n){this["UNSAFE_"+t]=n;}});}y.prototype.isReactComponent={};var J=n.vnode;n.vnode=function(n){n.$$typeof=S,function(t){var e=n.type,r=n.props;if(r&&"string"==typeof e){var o={};for(var u in r)/^on(Ani|Tra)/.test(u)&&(r[u.toLowerCase()]=r[u],delete r[u]),o[u.toLowerCase()]=u;if(o.ondoubleclick&&(r.ondblclick=r[o.ondoubleclick],delete r[o.ondoubleclick]),o.onbeforeinput&&(r.onbeforeinput=r[o.onbeforeinput],delete r[o.onbeforeinput]),o.onchange&&("textarea"===e||"input"===e.toLowerCase()&&!/^fil|che|ra/i.test(r.type))){var i=o.oninput||"oninput";r[i]||(r[i]=r[o.onchange],delete r[o.onchange]);}}}();var t=n.type;t&&t.t&&n.ref&&(n.props.ref=n.ref,n.ref=null),"function"==typeof t&&!t.p&&t.prototype&&(G(t.prototype,"componentWillMount"),G(t.prototype,"componentWillReceiveProps"),G(t.prototype,"componentWillUpdate"),t.p=!0),J&&J(n);};
 
     /*
     Copyright (c) 2018 Daybrush
@@ -604,6 +604,201 @@ version: 0.1.1
     }();
 
     /*
+    Copyright (c) 2019 Daybrush
+    name: preact-css-styler
+    license: MIT
+    author: Daybrush
+    repository: https://github.com/daybrush/css-styler/tree/master/preact-css-styler
+    version: 0.4.1
+    */
+
+    /*
+    Copyright (c) 2018 Daybrush
+    @name: @daybrush/utils
+    license: MIT
+    author: Daybrush
+    repository: https://github.com/daybrush/utils
+    @version 0.10.0
+    */
+    /**
+    * divide text by comma.
+    * @memberof Utils
+    * @param {string} text - text to divide
+    * @return {Array} divided texts
+    * @example
+    import {splitComma} from "@daybrush/utils";
+
+    console.log(splitComma("a,b,c,d,e,f,g"));
+    // ["a", "b", "c", "d", "e", "f", "g"]
+    console.log(splitComma("'a,b',c,'d,e',f,g"));
+    // ["'a,b'", "c", "'d,e'", "f", "g"]
+    */
+
+    function splitComma(text) {
+      // divide comma(,)
+      // "[^"]*"|'[^']*'
+      var matches = text.match(/("[^"]*"|'[^']*'|[^,\s()]*\((?:[^()]*|\([^()]*\))*\)[^,\s()]*|[^,])+/g);
+      return matches ? matches.map(function (str) {
+        return str.trim();
+      }) : [];
+    }
+
+    /*
+    Copyright (c) 2019 Daybrush
+    name: react-css-styler
+    license: MIT
+    author: Daybrush
+    repository: https://github.com/daybrush/css-styler/tree/master/react-css-styler
+    version: 0.4.0
+    */
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+
+    /* global Reflect, Promise */
+    var extendStatics$1 = function (d, b) {
+      extendStatics$1 = Object.setPrototypeOf || {
+        __proto__: []
+      } instanceof Array && function (d, b) {
+        d.__proto__ = b;
+      } || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+      };
+
+      return extendStatics$1(d, b);
+    };
+
+    function __extends$1(d, b) {
+      extendStatics$1(d, b);
+
+      function __() {
+        this.constructor = d;
+      }
+
+      d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    }
+    var __assign$2 = function () {
+      __assign$2 = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+
+          for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+
+        return t;
+      };
+
+      return __assign$2.apply(this, arguments);
+    };
+    function __rest$1(s, e) {
+      var t = {};
+
+      for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+
+      if (s != null && typeof Object.getOwnPropertySymbols === "function") for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
+      }
+      return t;
+    }
+
+    function hash(str) {
+      var hash = 5381,
+          i    = str.length;
+
+      while(i) {
+        hash = (hash * 33) ^ str.charCodeAt(--i);
+      }
+
+      /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+       * integers. Since we want the results to be always positive, convert the
+       * signed int to an unsigned by doing an unsigned bitshift. */
+      return hash >>> 0;
+    }
+
+    var stringHash = hash;
+
+    function getHash(str) {
+      return stringHash(str).toString(36);
+    }
+    function injectStyle(className, css) {
+      var style = document.createElement("style");
+      style.setAttribute("type", "text/css");
+      style.innerHTML = css.replace(/([^}{]*){/mg, function (all, selector) {
+        return splitComma(selector).map(function (subSelector) {
+          if (subSelector.indexOf(":global") > -1) {
+            return subSelector.replace(/\:global/g, "");
+          } else if (subSelector.indexOf(":host") > -1) {
+            return "" + subSelector.replace(/\:host/g, "." + className);
+          }
+
+          return "." + className + " " + subSelector;
+        }).join(", ") + "{";
+      });
+      (document.head || document.body).appendChild(style);
+      return style;
+    }
+
+    function styled(Tag, css) {
+      var injectClassName = "rCS" + getHash(css);
+      var injectCount = 0;
+      var injectElement;
+      return (
+        /*#__PURE__*/
+        function (_super) {
+          __extends$1(Styler, _super);
+
+          function Styler(props) {
+            return _super.call(this, props) || this;
+          }
+
+          Styler.prototype.render = function () {
+            var _a = this.props,
+                className = _a.className,
+                attributes = __rest$1(_a, ["className"]);
+
+            return W(Tag, __assign$2({
+              className: className + " " + injectClassName
+            }, attributes));
+          };
+
+          Styler.prototype.componentDidMount = function () {
+            if (injectCount === 0) {
+              injectElement = injectStyle(injectClassName, css);
+            }
+
+            ++injectCount;
+          };
+
+          Styler.prototype.componentWillUnmount = function () {
+            --injectCount;
+
+            if (injectCount === 0 && injectElement) {
+              injectElement.parentNode.removeChild(injectElement);
+            }
+          };
+
+          Styler.prototype.getElement = function () {
+            return this.element || (this.element = T$1(this));
+          };
+
+          return Styler;
+        }(y)
+      );
+    }
+
+    /*
     Copyright (c) 2018 Daybrush
     @name: @daybrush/utils
     license: MIT
@@ -674,7 +869,7 @@ version: 0.1.1
     license: MIT
     author: Daybrush
     repository: https://github.com/daybrush/guides/blob/master/packages/preact-guides
-    version: 0.1.2
+    version: 0.1.3
     */
 
     /*
@@ -702,8 +897,8 @@ version: 0.1.1
     ***************************************************************************** */
 
     /* global Reflect, Promise */
-    var extendStatics$1 = function (d, b) {
-      extendStatics$1 = Object.setPrototypeOf || {
+    var extendStatics$2 = function (d, b) {
+      extendStatics$2 = Object.setPrototypeOf || {
         __proto__: []
       } instanceof Array && function (d, b) {
         d.__proto__ = b;
@@ -711,11 +906,11 @@ version: 0.1.1
         for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
       };
 
-      return extendStatics$1(d, b);
+      return extendStatics$2(d, b);
     };
 
-    function __extends$1(d, b) {
-      extendStatics$1(d, b);
+    function __extends$2(d, b) {
+      extendStatics$2(d, b);
 
       function __() {
         this.constructor = d;
@@ -727,7 +922,7 @@ version: 0.1.1
     var Ruler =
     /*#__PURE__*/
     function (_super) {
-      __extends$1(Ruler, _super);
+      __extends$2(Ruler, _super);
 
       function Ruler() {
         var _this = _super !== null && _super.apply(this, arguments) || this;
@@ -1276,5 +1471,5 @@ version: 0.1.1
       guides2.scrollGuides(0);
     });
 
-}(styled));
+}());
 //# sourceMappingURL=index.js.map
