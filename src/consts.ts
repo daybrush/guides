aa572e48e6d48ea54ecda1174ae4fe4bf58ed23f
Daybrush <daybrush@gmail.com>
2019-11-11 11:09:52 +0900

    feat: add Vanilla Guides

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+import { GuidesProps } from "@scena/react-guides/declaration/types";
+
+export const PROPERTIES: Array<keyof GuidesProps> = [
+    "setGuides",
+    "type", "width", "height", "rulerStyle",
+    "unit", "zoom", "style", "backgroundColor", "lineColor",
+];
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Guides from "./Guides";
+
+export * from "./types";
+export * from "@scena/react-guides/declaration/types";
+export * from "./consts";
+export default Guides;
