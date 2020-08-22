4d8b4ce5ce4ad9222fa537c698755670a076b2f1
daybrush <daybrush@gmail.com>
2020-08-22 15:17:07 +0900

    fix: remove useles imports

--- packages/react-guides/src/react-guides/utils.ts
+++ packages/react-guides/src/react-guides/utils.ts
@@ -1,6 +1,4 @@
 import { prefixNames } from "framework-utils";
-import { splitBracket, splitComma } from "@daybrush/utils";
-import { mat4 } from "gl-matrix";
 
 export function prefix(...classNames: string[]) {
     return prefixNames("scena-", ...classNames);
