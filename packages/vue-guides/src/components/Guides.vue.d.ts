9f96d37a2c50e7f11432fa2fe4ab63aa31c7a866
Daybrush <daybrush@gmail.com>
2022-01-11 02:19:32 +0900

    fix: fix vue types #49

--- packages/vue-guides/src/components/Guides.vue.d.ts
+++ packages/vue-guides/src/components/Guides.vue.d.ts
@@ -1,15 +1,4 @@
-import { MethodInterface } from "framework-utils";
-import VanillaGuides, { GuideOptions, GuidesInterface } from "@scena/guides";
-
-interface VueGuidesInterface
-    extends MethodInterface<GuidesInterface, VanillaGuides, VueGuidesInterface> {
-    $el: HTMLElement;
-    $props: GuideOptions & { vueStyle: Record<string, any> };
-    $refs: any;
-    $emit(name: string, e: any): void;
-    $_guides: VanillaGuides;
-}
-
+import { VueGuidesInterface } from "./types";
 
 declare const VueGuides: VueGuidesInterface;
 type VueGuides = VueGuidesInterface;
